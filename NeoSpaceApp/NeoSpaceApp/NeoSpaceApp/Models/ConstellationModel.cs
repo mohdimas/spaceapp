c05eca8ea4f6f27b7b743c8b6abbae2f10d7b3b0
Abraham Ranardo Sumarsono <abrahamranardo@gmail.com>
2013-04-21 02:02:14 +0700

    Augmented Constellation v.0.1

---
+++ NeoSpaceApp/NeoSpaceApp/NeoSpaceApp/Models/ConstellationModel.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace NeoSpaceApp.Models
+{
+    class ConstellationModel
+    {
+        //  Variables used as parameters: must be predefined here!
+        public double ra = 0.7408745, dec = 39.8714421;
+        public double angle = 45.0;
+        public int dataset = 0, linedataset = 2, altlinedataset = 3; // dataset: 0 bsc, 1 ybs
+
+        // Global variables
+        public double[,] stars, stars_math;
+        public int numstars, usestars, loadedlists;
+        public int[] starlists;
+        public int width, height;
+        public double[,] lines, lines_math;
+        public int numlines;
+        public double[,] altlines, altlines_math;
+        public int altnumlines;
+        public bool uselines, usealtlines;
+        public String[] constnames;
+        public double[,] constcoords, constcoords_math;
+        public int numconst;
+        public bool usenames;
+
+        public ConstellationModel(int _width, int _height)
+        {
+            width = _width;
+            height = _height;
+
+            ra = ra * Math.PI / 12.0;
+            dec = dec * Math.PI / 180;
+            angle = angle * Math.PI / 180;
+
+            stars = new double[10000,3];
+            stars_math = new double[10000,4];
+            starlists = new int[7];
+            numstars = 0;
+            usestars = 0;
+            lines = new double[1000,4];
+            lines_math = new double[1000,8];
+            numlines = 0;
+            altlines = new double[1000,4];
+            altlines_math = new double[1000,8];
+            altnumlines = 0;
+            constnames = new String[100];
+            constcoords = new double[100,2];
+            constcoords_math = new double[100,4];
+            numconst = 0;
+            uselines = true;
+            usealtlines = true;
+            usenames = true;
+        }
+
+        public void LoadData()
+        {
+            getData(1);
+            getData(2);
+            getConsts();
+            getData(3);
+            getStdLines();
+            getData(4);
+            getAltLines();
+            getData(5);
+            getData(6);
+            getData(7);
+        }
+
+        public void getConsts() 
+        {
+            int i = 0, j = 0;
+
+            try
+            {
+                var s = "data/averagebdys.dat";
+                var si = Application.GetResourceStream(new Uri(s, UriKind.Relative));
+                if (si != null)
+                {
+                    using (var reader = new StreamReader(si.Stream))
+                    {
+                        var content = reader.ReadToEnd();
+                        var contents = content.Split(new string[] { " ", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                        foreach (var c in contents)
+                        {
+                            if (i == 0)
+                            {
+                                constnames[j] = c;
+                            }
+                            else if (i == 1)
+                            {
+                                constcoords[j,0] = double.Parse(c) * Math.PI / 12.0;
+                                constcoords_math[j,0] = Math.Sin(constcoords[j,0]);
+                                constcoords_math[j,1] = Math.Cos(constcoords[j,0]);
+                            }
+                            else
+                            {
+                                constcoords[j, 1] = double.Parse(c) * Math.PI / 180.0;
+                                constcoords_math[j,2] = Math.Sin(constcoords[j,1]);
+                                constcoords_math[j,3] = Math.Cos(constcoords[j,1]);
+                                j++;
+                                i = -1;
+                            }
+                            i++;
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                numconst = 0;
+                usenames = false;
+                return;
+            }
+
+            numconst = j;
+            //deliverEvent(new Event(this, theApp.NEW_STUFF, this));
+        }
+
+        public void getStdLines()
+        {
+            numlines = getLines(linedataset);
+            //theApp.newStuff();
+        }
+
+        public void getAltLines()
+        {
+            if (linedataset == altlinedataset)
+            {
+                if (numlines == 0)
+                {
+                    uselines = false;
+                }
+            }
+            else
+            {
+                altnumlines = getAltLines(altlinedataset);
+                if (numlines == 0)
+                {
+                    usealtlines = true;
+                    if (altnumlines == 0)
+                    {
+                        uselines = false;
+                    }
+                }
+                //deliverEvent(new Event(this, NEW_STUFF, this));
+            }
+        }
+
+        public int getAltLines(int catalog)
+        {
+            int i = 0, j = 0;
+            String s;
+
+            try
+            {
+                if (catalog == 0)
+                    s = "data/lines.dat";
+                else
+                    s = "data/lines" + catalog + ".dat";
+
+                var si = Application.GetResourceStream(new Uri(s, UriKind.Relative));
+                if (si != null)
+                {
+                    using (var reader = new StreamReader(si.Stream))
+                    {
+                        var content = reader.ReadToEnd();
+                        var contents = content.Split(new string[] { " ", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                        foreach (var c in contents)
+                        {
+                            altlines[j, i] = double.Parse(c);
+                            altlines_math[j, 2 * i] = Math.Sin(double.Parse(c));
+                            altlines_math[j, 2 * i + 1] = Math.Cos(double.Parse(c));
+                            if ((++i) == 4)
+                            {
+                                j++;
+                                i = 0;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+
+            return j;
+        }
+
+        public int getLines(int catalog) {
+            int i = 0, j = 0;
+            String s;
+
+            try
+            {
+                if (catalog == 0)
+                    s = "data/lines.dat";
+                else
+                    s = "data/lines" + catalog + ".dat";
+
+                var si = Application.GetResourceStream(new Uri(s, UriKind.Relative));
+                if (si != null)
+                {
+                    using (var reader = new StreamReader(si.Stream))
+                    {
+                        var content = reader.ReadToEnd();
+                        var contents = content.Split(new string[] { " ", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                        foreach (var c in contents)
+                        {
+                            lines[j,i] = double.Parse(c);
+                            lines_math[j, 2 * i] = Math.Sin(double.Parse(c));
+                            lines_math[j, 2 * i + 1] = Math.Cos(double.Parse(c));
+                            if ((++i) == 4)
+                            {
+                                j++;
+                                i = 0;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+
+            return j;
+        }
+
+        public void getData(int n) 
+        {
+            int i;
+            String url;
+
+            switch (dataset) {
+                case 0:
+                    if (n == 0)
+                        url = "data/bsc.dat";
+                    else
+                        url = "data/bsc" + n + ".dat";
+                    break;
+
+                case 1:
+                    if (n == 0)
+                        url = "data/YBS.dat";
+                    else
+                        url = "data/YBS" + n + ".dat";
+                    break;
+
+                default:
+                    //Console.Out.WriteLine("Invalid data set requested");
+                    return;
+            }
+
+            i = getFile(url, numstars);
+            if (i > 0) {
+                numstars += i; 
+                starlists[loadedlists++] = numstars;
+            }
+        }
+
+        public int getFile(String s, int n) {
+            int i, j = 0;
+            try
+            {
+                var si = Application.GetResourceStream(new Uri(s, UriKind.Relative));
+                if (si != null)
+                {
+                    using (var reader = new StreamReader(si.Stream))
+                    {
+                        var content = reader.ReadToEnd();
+                        var contents = content.Split(new string[] { " ", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                        i = 0;
+                        j = 0;
+                        foreach (var c in contents)
+                        {
+                            stars[n + j, i] = double.Parse(c);
+                            if (i != 2)
+                            {
+                                stars_math[n + j, 2 * i] = Math.Sin(double.Parse(c));
+                                stars_math[n + j, 2 * i + 1] = Math.Cos(double.Parse(c));
+                            }
+                            if ((++i) == 3)
+                            {
+                                j++;
+                                i = 0;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+            
+            return j;
+        }
+    }
+}
--- NeoSpaceApp/NeoSpaceApp/NeoSpaceApp/Views/GamePage.xaml.cs
+++ NeoSpaceApp/NeoSpaceApp/NeoSpaceApp/Views/GamePage.xaml.cs
@@ -12,6 +12,9 @@ using Microsoft.Phone.Controls;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using NeoSpaceApp.Models;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Devices.Sensors;
 
 namespace NeoSpaceApp
 {
@@ -20,6 +23,17 @@ namespace NeoSpaceApp
         ContentManager contentManager;
         GameTimer timer;
         SpriteBatch spriteBatch;
+
+        SpriteFont myFont;
+        ConstellationModel model;
+        double horPos;
+        double verPos;
+
+        Motion motion;
+        bool IsMotion;
+
+        private double[] brightnesses = { 1.0, 0.9, 0.7, 1.0, 0.7, 0.4, 0.1 };
+        private int xmid, ymid;
 
         public GamePage()
         {
@@ -33,6 +47,20 @@ namespace NeoSpaceApp
             timer.UpdateInterval = TimeSpan.FromTicks(333333);
             timer.Update += OnUpdate;
             timer.Draw += OnDraw;
+
+            this.Width = System.Windows.Application.Current.Host.Content.ActualWidth;
+            this.Height = System.Windows.Application.Current.Host.Content.ActualHeight;
+
+            model = new ConstellationModel((int)this.Width, (int)this.Height);
+            model.LoadData();
+
+            verPos = (-model.dec) * 180.0 / Math.PI;
+            horPos = (360.0 - model.ra * 180.0 / Math.PI);
+
+            xmid = (int)this.Width / 2;
+            ymid = (int)this.Height / 2;
+
+            IsMotion = Motion.IsSupported;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -44,17 +72,49 @@ namespace NeoSpaceApp
             spriteBatch = new SpriteBatch(SharedGraphicsDeviceManager.Current.GraphicsDevice);
 
             // TODO: use this.content to load your game content here
+            myFont = contentManager.Load<SpriteFont>("SpriteFontNeo");
 
             // Start the timer
             timer.Start();
 
+            // If the Motion object is null, initialize it and add a CurrentValueChanged
+            // event handler.
+            if (Motion.IsSupported)
+            {
+                if (motion == null)
+                {
+                    motion = new Motion();
+                    motion.TimeBetweenUpdates = TimeSpan.FromTicks(333333);
+                    motion.CurrentValueChanged += motion_CurrentValueChanged;
+                }
+
+                // Try to start the Motion API.
+                try
+                {
+                    motion.Start();
+                }
+                catch (Exception)
+                {
+                    IsMotion = false;
+                }
+            }
+
             base.OnNavigatedTo(e);
+        }
+
+        void motion_CurrentValueChanged(object sender, SensorReadingEventArgs<MotionReading> e)
+        {
+            verPos = MathHelper.ToDegrees(e.SensorReading.Attitude.Pitch);
+            horPos = MathHelper.ToDegrees(e.SensorReading.Attitude.Yaw);
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
             // Stop the timer
             timer.Stop();
+
+            if (motion != null)
+                motion.Stop();
 
             // Set the sharing mode of the graphics device to turn off XNA rendering
             SharedGraphicsDeviceManager.Current.GraphicsDevice.SetSharingMode(false);
@@ -69,6 +129,40 @@ namespace NeoSpaceApp
         private void OnUpdate(object sender, GameTimerEventArgs e)
         {
             // TODO: Add your update logic here
+            if (!IsMotion)
+            {
+                foreach (TouchLocation location in TouchPanel.GetState())
+                {
+                    TouchLocation prev;
+                    if (location.TryGetPreviousLocation(out prev))
+                    {
+                        if (location.Position.X - prev.Position.X != 0)
+                        {
+                            horPos += (prev.Position.X - location.Position.X) * 0.1;
+
+                            if (horPos > 360)
+                                horPos = 0;
+
+                            if (horPos < 0)
+                                horPos = 360;
+                        }
+
+                        if (location.Position.Y - prev.Position.Y != 0)
+                        {
+                            verPos += (prev.Position.Y - location.Position.Y) * 0.1;
+
+                            if (verPos > 360)
+                                verPos = 0;
+
+                            if (verPos < 0)
+                                verPos = 360;
+                        }
+                    }
+                }
+            }
+
+            model.ra = (360.0 - horPos) * Math.PI / 180.0;
+            model.dec = -verPos * Math.PI / 180.0;
         }
 
         /// <summary>
@@ -76,9 +170,245 @@ namespace NeoSpaceApp
         /// </summary>
         private void OnDraw(object sender, GameTimerEventArgs e)
         {
-            SharedGraphicsDeviceManager.Current.GraphicsDevice.Clear(Color.CornflowerBlue);
+            SharedGraphicsDeviceManager.Current.GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            if (model.uselines)
+                if (model.usealtlines)
+                    PaintAltLines();
+                else
+                    PaintLines();
+
+            //use all stars
+            model.usestars = model.starlists[model.loadedlists - 2];
+            PaintStars();
+
+            if (model.usenames)
+                PaintNames();
+            spriteBatch.End();
+        }
+
+        private void DrawCenteredString(String s, int x, int y, Color? color = null)
+        {
+            if (color == null)
+                color = Color.Red;
+
+            var si = myFont.MeasureString(s);
+            spriteBatch.DrawString(myFont, s, new Vector2(x - (si.X / 2), y - (si.Y / 2)), (Color)color);
+        }
+
+        private void DrawLine(Vector2 start, Vector2 end, Color? color = null)
+        {
+            if (color == null)
+                color = Color.Blue;
+
+            var canvas = new Texture2D(SharedGraphicsDeviceManager.Current.GraphicsDevice, 1, 1, false,
+    SurfaceFormat.Color);
+            canvas.SetData<Color>(new[] { (Color)color });
+
+            spriteBatch.Draw(canvas, start, null, (Color) color,
+                         (float)Math.Atan2(end.Y - start.Y, end.X - start.X),
+                         new Vector2(0f, (float)canvas.Height / 2),
+                         new Vector2(Vector2.Distance(start, end), 1f),
+                         SpriteEffects.None, 0f);
+        }
+
+        private void PaintNames()
+        {
+            int i, x, y;
+            double dist, ex, ey, ez;
+            double cr, sr, cd, sd, cr0, sr0, cd0, sd0;
+
+            dist = (double)Math.Min(xmid, ymid) / Math.Tan(model.angle / 2.0);
+
+            sr = Math.Sin(model.ra);
+            cr = Math.Cos(model.ra);
+            sd = Math.Sin(model.dec);
+            cd = Math.Cos(model.dec);
+
+            for (i = model.numconst - 1; i >= 0; i--)
+            {
+                sr0 = model.constcoords_math[i,0];
+                cr0 = model.constcoords_math[i,1];
+                sd0 = model.constcoords_math[i,2];
+                cd0 = model.constcoords_math[i,3];
+                ez = cd * cd0 * (cr * cr0 + sr * sr0) + sd * sd0;
+                if (ez > 0.1)
+                {
+                    ey = -sd * cd0 * (cr * cr0 + sr * sr0) + cd * sd0;
+                    ex = -cd0 * (sr * cr0 - cr * sr0);
+                    x = xmid + (int)(-dist * ex / ez);
+                    y = ymid + (int)(-dist * ey / ez);
+
+                    var offset = 100;
+                    if (x >= 0 - offset && x <= this.Width + offset && y >= 0 - offset && y <= this.Height + offset)
+                        DrawCenteredString(model.constnames[i], x, y);
+                }
+            }
+        }
+
+        private void PaintStars()
+        {
+            int i, x, y;
+            double dist, ex, ey, ez;
+            double cr, sr, cd, sd, cr0, sr0, cd0, sd0;
+            int b;
+
+            dist = (double)Math.Min(xmid, ymid) / Math.Tan(model.angle / 2.0);
+
+            sr = Math.Sin(model.ra);
+            cr = Math.Cos(model.ra);
+            sd = Math.Sin(model.dec);
+            cd = Math.Cos(model.dec);
+
+            for (i = model.usestars - 1; i >= 0; i--)
+            {
+                b = (int)model.stars[i,2];
+                b = b < 0 ? 0 : (b > 6 ? 6 : b);
+                var color = new Color((float)255.0, (float)255.0, (float)255.0);
+                color = color * (float)brightnesses[b];
+
+                /* Projected view */
+                /*
+                      ex = -cos(top.stars[i][1]) * (sin(top.ra)*cos(top.stars[i][0]) - 
+                                   cos(top.ra)*sin(top.stars[i][0]));
+                      ey = -cos(top.stars[i][1])*sin(top.dec) * 
+                            (cos(top.ra)*cos(top.stars[i][0]) + 
+                             sin(top.ra)*sin(top.stars[i][0]) + 
+                        cos(top.stars[i][1])*cos(top.dec);
+                      ez = cos(top.stars[i][1])*cos(top.dec) * 
+                            (cos(top.ra)*cos(top.stars[i][0]) + 
+                             sin(top.ra)*sin(top.stars[i][0]) + 
+                        sin(top.stars[i][1])*sin(top.dec);
+                */
+
+                sr0 = model.stars_math[i,0];
+                cr0 = model.stars_math[i,1];
+                sd0 = model.stars_math[i,2];
+                cd0 = model.stars_math[i,3];
+
+                ez = cd * cd0 * (cr * cr0 + sr * sr0) + sd * sd0;
+
+                if (ez > 0.1)
+                {
+
+                    ey = -sd * cd0 * (cr * cr0 + sr * sr0) + cd * sd0;
+                    ex = -cd0 * (sr * cr0 - cr * sr0);
+
+                    x = xmid + (int)(-dist * ex / ez);
+                    y = ymid + (int)(-dist * ey / ez);
+
+                    if (x >= 0 && x <= this.Width && y >= 0 && y <= this.Height)
+                    {
+                        switch (b)
+                        {
+                            case 0:
+                                DrawLine(new Vector2(x - 2, y), new Vector2(x + 2, y), color);
+                                DrawLine(new Vector2(x, y - 2), new Vector2(x, y + 2), color);
+                                break;
+                            case 1:
+                            case 2:
+                                DrawLine(new Vector2(x, y), new Vector2(x + 2, y), color);
+                                DrawLine(new Vector2(x, y - 2), new Vector2(x + 2, y - 2), color);
+                                break;
+                            default:
+                                DrawLine(new Vector2(x - 1, y), new Vector2(x + 1, y), color);
+                                DrawLine(new Vector2(x, y - 1), new Vector2(x, y + 1), color);
+                                //DrawLine(new Vector2(x, y), new Vector2(x, y), color);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void PaintAltLines()
+        {
+            int i, x1, x2, y1, y2;
+            double dist, ex1, ex2, ey1, ey2, ez1, ez2;
+            double cr, sr, cd, sd, cr1, sr1, cd1, sd1, cr2, sr2, cd2, sd2;
+
+            dist = (double)Math.Min(xmid, ymid) / Math.Tan(model.angle / 2.0);
+            sr = Math.Sin(model.ra);
+            cr = Math.Cos(model.ra);
+            sd = Math.Sin(model.dec);
+            cd = Math.Cos(model.dec);
+
+            for (i = model.altnumlines - 1; i >= 0; i--)
+            {
+                sr1 = model.altlines_math[i,0];
+                cr1 = model.altlines_math[i,1];
+                sd1 = model.altlines_math[i,2];
+                cd1 = model.altlines_math[i,3];
+                sr2 = model.altlines_math[i,4];
+                cr2 = model.altlines_math[i,5];
+                sd2 = model.altlines_math[i,6];
+                cd2 = model.altlines_math[i,7];
+                ez1 = cd * cd1 * (cr * cr1 + sr * sr1) + sd * sd1;
+                ez2 = cd * cd2 * (cr * cr2 + sr * sr2) + sd * sd2;
+                if (ez1 > 0.1 && ez2 > 0.1)
+                {
+                    ey1 = -sd * cd1 * (cr * cr1 + sr * sr1) + cd * sd1;
+                    ey2 = -sd * cd2 * (cr * cr2 + sr * sr2) + cd * sd2;
+                    ex1 = -cd1 * (sr * cr1 - cr * sr1);
+                    ex2 = -cd2 * (sr * cr2 - cr * sr2);
+
+                    x1 = xmid + (int)(-dist * ex1 / ez1);
+                    x2 = xmid + (int)(-dist * ex2 / ez2);
+                    y1 = ymid + (int)(-dist * ey1 / ez1);
+                    y2 = ymid + (int)(-dist * ey2 / ez2);
+                    
+                    if ((x1 >= 0 && x1 <= this.Width && y1 >= 0 && y1 <= this.Height) || (x2 >= 0 && x2 <= this.Width && y2 >= 0 && y2 <= this.Height))
+                    {
+                        DrawLine(new Vector2(x1, y1), new Vector2(x2, y2));
+                    }
+                }
+            }
+        }
+
+        private void PaintLines()
+        {
+            int i, x1, x2, y1, y2;
+            double dist, ex1, ex2, ey1, ey2, ez1, ez2;
+            double cr, sr, cd, sd, cr1, sr1, cd1, sd1, cr2, sr2, cd2, sd2;
+
+            dist = (double)Math.Min(xmid, ymid) / Math.Tan(model.angle / 2.0);
+            sr = Math.Sin(model.ra);
+            cr = Math.Cos(model.ra);
+            sd = Math.Sin(model.dec);
+            cd = Math.Cos(model.dec);
+
+            for (i = model.numlines - 1; i >= 0; i--)
+            {
+                sr1 = model.lines_math[i,0];
+                cr1 = model.lines_math[i,1];
+                sd1 = model.lines_math[i,2];
+                cd1 = model.lines_math[i,3];
+                sr2 = model.lines_math[i,4];
+                cr2 = model.lines_math[i,5];
+                sd2 = model.lines_math[i,6];
+                cd2 = model.lines_math[i,7];
+                ez1 = cd * cd1 * (cr * cr1 + sr * sr1) + sd * sd1;
+                ez2 = cd * cd2 * (cr * cr2 + sr * sr2) + sd * sd2;
+                if (ez1 > 0.1 && ez2 > 0.1)
+                {
+                    ey1 = -sd * cd1 * (cr * cr1 + sr * sr1) + cd * sd1;
+                    ey2 = -sd * cd2 * (cr * cr2 + sr * sr2) + cd * sd2;
+                    ex1 = -cd1 * (sr * cr1 - cr * sr1);
+                    ex2 = -cd2 * (sr * cr2 - cr * sr2);
+
+                    x1 = xmid + (int)(-dist * ex1 / ez1);
+                    x2 = xmid + (int)(-dist * ex2 / ez2);
+                    y1 = ymid + (int)(-dist * ey1 / ez1);
+                    y2 = ymid + (int)(-dist * ey2 / ez2);
+
+                    if ((x1 >= 0 && x1 <= this.Width && y1 >= 0 && y1 <= this.Height) || (x2 >= 0 && x2 <= this.Width && y2 >= 0 && y2 <= this.Height))
+                    {
+                        DrawLine(new Vector2(x1, y1), new Vector2(x2, y2));
+                    }
+                }
+            }
         }
     }
 }
